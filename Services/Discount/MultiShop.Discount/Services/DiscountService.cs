9c057e95867f3d4a62ffbfb83b28ddc960c7fa17
undemir-mms <166118231+undemir-mms@users.noreply.github.com>
2024-05-23 16:15:51 +0300

    DiscountService

--- Services/Discount/MultiShop.Discount/Services/DiscountService.cs
+++ Services/Discount/MultiShop.Discount/Services/DiscountService.cs
@@ -1,6 +1,32 @@
-﻿namespace MultiShop.Discount.Services
+﻿using MultiShop.Discount.Dtos;
+
+namespace MultiShop.Discount.Services
 {
     public class DiscountService : IDiscountService
     {
+        public Task CreateCouponAsync(CreateCouponDto createCouponDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCouponAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultCouponDto>> GetAllCouponAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetByIdCouponDto> GetCouponByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCouponAsync(UpdateCouponDto updateCouponDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
